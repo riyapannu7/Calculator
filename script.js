b786f8b3473b62e462e9dd7e8242ae8ed9f2542e
Riya Pannu <riyapannu7@gmail.com>
2017-10-07 17:03:41 +0530

    Uploading code

---
+++ script.js
@@ -0,0 +1,175 @@
+
+    
+    var num1=document.getElementById('num1');
+var num2=document.getElementById('num2');
+var num3=document.getElementById('num3');
+var num4=document.getElementById('num4');
+var num5=document.getElementById('num5');
+var num6=document.getElementById('num6');
+var num7=document.getElementById('num7');
+var num8=document.getElementById('num8');
+var num9=document.getElementById('num9');
+
+var o1=document.getElementById('o1');
+var o2=document.getElementById('o2');
+var o3=document.getElementById('o3');
+var o4=document.getElementById('o4');
+
+    var out=document.getElementById('out');
+    
+    
+    num1.onclick = function(){
+        out.innerHTML += 1;
+    };
+num2.onclick = function(){
+        out.innerHTML += 2;
+    };
+num3.onclick = function(){
+        out.innerHTML += 3;
+    };
+num4.onclick = function(){
+        out.innerHTML += 4;
+    };
+num5.onclick = function(){
+        out.innerHTML += 5;
+    };
+num6.onclick = function(){
+        out.innerHTML += 6;
+    };
+num7.onclick = function(){
+        out.innerHTML += 7;
+    };
+num8.onclick = function(){
+        out.innerHTML += 8;
+    };
+num9.onclick = function(){
+        out.innerHTML += 9;
+    };
+
+
+
+o1.onclick=function(){
+    num1.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)+1;  
+    }
+    num2.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)+2;  
+    }
+    num3.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)+3;  
+    }
+    num4.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)+4;  
+    }
+    num5.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)+5;  
+    }
+    num6.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)+6;  
+    }
+    num7.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)+7;  
+    }
+    num8.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)+8;  
+    }
+    num9.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)+9;  
+    }
+};
+
+o2.onclick=function(){
+    num1.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)-1;  
+    }
+    num2.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)-2;  
+    }
+    num3.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)-3;  
+    }
+    num4.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)-4;  
+    }
+    num5.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)-5;  
+    }
+    num6.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)-6;  
+    }
+    num7.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)-7;  
+    }
+    num8.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)-8;  
+    }
+    num9.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)-9;  
+    }
+};
+
+
+o3.onclick=function(){
+    num1.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)*1;  
+    }
+    num2.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)*2;  
+    }
+    num3.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)*3;  
+    }
+    num4.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)*4;  
+    }
+    num5.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)*5;  
+    }
+    num6.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)*6;  
+    }
+    num7.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)*7;  
+    }
+    num8.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)*8;  
+    }
+    num9.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)*9;  
+    }
+};
+
+
+o4.onclick=function(){
+    num1.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)/1;  
+    }
+    num2.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)/2;  
+    }
+    num3.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)/3;  
+    }
+    num4.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)/4;  
+    }
+    num5.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)/5;  
+    }
+    num6.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)/6;  
+    }
+    num7.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)/7;  
+    }
+    num8.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)/8;  
+    }
+    num9.onclick=function(){
+      out.innerHTML=parseInt(out.innerHTML)/9;  
+    }
+};
+
+
+
+    
